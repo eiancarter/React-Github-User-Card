16d0a41b56be6faf3f0c67df524a275f8d9a01f5
Eian Carter <eian@bricleir.com>
2020-01-14 12:12:37 -0800

    added file structure

---
+++ github-usercard-a/src/components/GitCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GitCard = () => {
+    return (
+        <div>
+            Git Card
+        </div>
+    )
+}
+
+export default GitCard;
---
+++ github-usercard-a/src/components/GitList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GitCard from './GitCard';
+
+const GitList = () => {
+    return (
+        <div>
+            <GitCard />
+        </div>
+    )
+}
+
+export default GitList;
