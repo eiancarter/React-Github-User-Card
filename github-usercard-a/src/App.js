bd21712e34c2fe35a26d772e30a5a7f6c4b2d838
Eian Carter <eian@bricleir.com>
2020-01-14 16:01:42 -0800

    met mvp

--- github-usercard-a/src/App.js
+++ github-usercard-a/src/App.js
@@ -1,33 +1,58 @@
 import React, { Component } from 'react'; 
 import './App.css';
 import GitList from './components/GitList';
+import GitCard from './components/GitCard';
 import axios from 'axios';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      cards: []
+      cards: [],
+      followers: []
     }
     console.log('constructor works!')
   }
 
   componentDidMount(){
     axios
-      .get(`https://api.github.com/users/${username}`)
+      .get(`https://api.github.com/users/eiancarter`)
       .then(response => {
         this.setState({
           cards: response.data
         })
-      }
+      })
       .catch(error => {
         console.log('nope!', error);
       });
+
+    axios
+    .get(`https://api.github.com/users/eiancarter/followers`)
+    .then(response => {
+      console.log(response)
+      this.setState({
+        followers: response.data
+      });
+    })
+    .catch( error => {
+      console.log('try again!', error);
+    })
   }
+
+  componentWillUnmount() {
+  }
+
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('update is woking!', prevState.cards, this.state.cards);
+    console.log('followers is working!', this.state.followers);
+  }
+
   render() {
     return (
       <div className="App">
-        <GitList />
+        <GitCard cards={this.state.cards} />
+        <GitList followers={this.state.followers} />
       </div>
     );
   }
--- github-usercard-a/src/components/GitCard.js
+++ github-usercard-a/src/components/GitCard.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const GitCard = () => {
+const GitCard = props => {
     return (
-        <div>
-            Git Card
+        <div className='git-card'>
+            <img className='card-img' src={props.cards.avatar_url} alt={props.cards.name} />
+            <h2>{props.cards.name}</h2>
+            <h3>{props.cards.location}</h3>
+            <h3>Bio:</h3>
+                <p>{props.cards.bio}</p>
+            <h3>Followers: {props.cards.followers}</h3>
+            <h3>Following: {props.cards.following}</h3>
         </div>
     )
 }
